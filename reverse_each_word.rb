67258aef1b6544b340e4ebdffabe8a80bbfd20e2
Michelle M <michellemoon236@gmail.com>
2019-03-20 00:06:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@
 
   def reverse_each_word (sentence)
   reverse_sentence = [] 
-  sentence.split(" ").collect do |word|
+  sentence.split.collect do |word|
     reverse_sentence << word.reverse 
   end
-  reverse_sentence.join(" ")
+
 end
